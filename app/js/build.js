2fab9e88ac93e7f11f7fdeb6ec02b3b1870dcd0a
gemaderus <hi@gemaderus.com>
2018-04-06 12:42:59 +0200

    Removed app folder

--- app/js/build.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-$("[data-theme]").on("click", function () {
-    var theme = $(this).attr("data-theme");
-    $("body").removeClass().addClass(theme);
-});
-//# sourceMappingURL=build.js.map
